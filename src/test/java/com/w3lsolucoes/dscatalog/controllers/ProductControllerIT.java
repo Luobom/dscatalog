5f2dd747f43917974283f2eb00a248f9b43c117b
Luciano Nascimento <luciano.m.nasc@gmail.com>
2024-10-09 19:50:02 -0300

    a few adjusts

--- src/test/java/com/w3lsolucoes/dscatalog/controllers/ProductControllerIT.java
+++ src/test/java/com/w3lsolucoes/dscatalog/controllers/ProductControllerIT.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -20,6 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 public class ProductControllerIT {
 
     @Autowired
